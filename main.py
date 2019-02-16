253b3ce31ec0c3bf9bcc54431a2030f764dfd96d
dshkabatur <dshkabatur@yandex.ru>
2019-02-16 14:50:35 +0300

    make code refactoring

--- main.py
+++ main.py
@@ -8,50 +8,47 @@ import time
 username = "admin"
 password = "T$2z-artek"
 
-audag = {
-    'device_type' : 'eltex',
-    'ip':   '10.55.15.140',
-    'username': username,
-    'password': password, }
 
-boss = {
-    'device_type' : 'eltex',
-    'ip':   '172.16.13.222',
-    'username': username,
-    'password': password, }
+clusters = {
+    'audag': {
+        'ip' : '10.55.15.140',
+        'nodes': [],
+    },
+    'guest': {
+        'ip' : '10.55.13.111',
+        'nodes': [],
+    },
+    'boss': {
+        'ip' : '172.16.13.222',
+        'nodes': [],
+    }
+}
 
 
-guest = {
-   'device_type' : 'eltex',
-   'ip':   '10.55.13.111',
-   'username': username,
-   'password': password, }
-
-guest_cn_raw = ConnectHandler(**guest).send_command(
+def parse_clusternodes(ip):
+    cluster = {
+        'device_type' : 'eltex',
+        'ip':   ip,
+        'username': username,
+        'password': password, }
+    
+    raw_cn = ConnectHandler(**cluster).send_command(
     'get cluster-member detail').split('Property            Value\n''-------------------------------------\n')[1:]
-audag_cn_raw = ConnectHandler(**audag).send_command(
-    'get cluster-member detail').split('Property            Value\n''-------------------------------------\n')[1:]
-
-guest_cluster_nodes = []
-audag_cluster_nodes = []
-
-node = {} 
-for n in guest_cn_raw:
-    for params in n.split('\n'):
-        p = params.split()
-        if(p):
-            node[p[0]] = p[1]
-    guest_cluster_nodes.append(node)
+    cluster_nodes = []
     node = {}
+    for n in raw_cn:
+        for params in n.split('\n'):
+            p = params.split()
+            if(len(p) > 1):
+                node[p[0]] = p[1]
+        cluster_nodes.append(node)
+        node = {}
+    return cluster_nodes
 
-node = {} 
-for n in audag_cn_raw:
-    for params in n.split('\n'):
-        p = params.split()
-        if(p):
-            node[p[0]] = p[1]
-    audag_cluster_nodes.append(node)
-    node = {}
+for key in clusters:
+    clusters[key]['nodes'] = parse_clusternodes(clusters[key]['ip'])
+
+
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -73,27 +70,9 @@ def reboot():
    connect_handler = ConnectHandler(**node_to_reboot).send_command('reboot')
    return str(r)
 
-@app.route('/audag')
-def get_audag():
-    #копируем в переменную nodes точки в кластере 
-    nodes = audag_cluster_nodes.copy()
-    #создаем массив с ip'шниками для multi_ping
-    ips = [ n['ip'] for n in nodes]
-    resp, no_resp = multi_ping(ips, timeout=2, retry=3)
-    for n in nodes:
-       if n['ip'] in resp:
-          n['ping'] = resp[n['ip']] * 1000
-          n['status'] = True
-       else:
-          n['ping'] = 9999
-          n['status'] = False
-    return jsonify(nodes)
-
-@app.route('/guest')
-def get_guest():
-    #копируем в переменную nodes точки в кластере 
-    nodes = guest_cluster_nodes.copy()
-    #создаем массив с ip'шниками для multi_ping
+@app.route('/<name>')
+def get_cluster_nodes(name):
+    nodes = clusters[name]['nodes'].copy()
     ips = [ n['ip'] for n in nodes]
     resp, no_resp = multi_ping(ips, timeout=2, retry=3)
     for n in nodes:
